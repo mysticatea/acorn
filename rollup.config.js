d3a2faacdc86724588ff4f44805e26758f119804
Toru Nagashima <public@mysticatea.dev>
2021-04-22 12:48:17 +0900

    build

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import buble from "rollup-plugin-buble"
+
+export default {
+  input: "acorn/src/index.js",
+  output: [
+    {
+      file: "acorn/dist/acorn.js",
+      format: "umd",
+      name: "acorn"
+    },
+    {
+      file: "acorn/dist/acorn.mjs",
+      format: "es"
+    }
+  ],
+  plugins: [
+    buble({transforms: {dangerousForOf: true}})
+  ]
+}
